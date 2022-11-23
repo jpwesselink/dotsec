8b123d364b629a47c9df20ac48998bae4611f905
JP Wesselink <jpwesselink@gmail.com>
2022-11-23 22:55:48 +0100

    v1.0.0-alpha.7

--- packages/dotsec/tsup.config.ts
+++ packages/dotsec/tsup.config.ts
@@ -3,8 +3,10 @@ import type { Options } from "tsup";
 export const tsup: Options = {
 	splitting: false,
 	sourcemap: true,
+	minify: true,
 	clean: true,
 	dts: true,
 	format: ["cjs", "esm"],
+	skipNodeModulesBundle: true,
 	entryPoints: ["src/cli/index.ts", "src/index.ts"],
 };
